50d7319c055135371b4ade3a5b01f8259fe1fde6
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-05-24 13:57:35 +0200

    [Footer] style: introduce style to the component

--- components/Pages/Footer/Footer.page.tsx
+++ components/Pages/Footer/Footer.page.tsx
@@ -11,132 +11,137 @@ import githubIcon from "@/assets/github_icon.svg";
 function Footer() {
   return (
     <footer className={s.container}>
-      <section className={s.container__logoSection}>
-        <Image
-          className={s.container__logoSection__img}
-          src={blogLogo}
-          alt="Blog's logo"
-        />
+      <section className={s.container__content}>
+        <section className={s.container__logoSection}>
+          <Image
+            className={s.container__logoSection__img}
+            src={blogLogo}
+            alt="Blog's logo"
+          />
+        </section>
+        <section className={s.container__linksSection}>
+          <section className={s.container__linksSection__linksWrapper}>
+            <div
+              className={s.container__linksSection__linksWrapper__sectionHeader}
+            >
+              <h3
+                className={
+                  s.container__linksSection__linksWrapper__sectionHeader__header
+                }
+              >
+                Linki
+              </h3>
+            </div>
+            <div
+              className={
+                s.container__linksSection__linksWrapper__sectionElementsWrapper
+              }
+            >
+              <ul
+                className={
+                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems
+                }
+              >
+                <li
+                  className={
+                    s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
+                  }
+                >
+                  Oferty Pracy
+                </li>
+                <li
+                  className={
+                    s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
+                  }
+                >
+                  Kursy IT
+                </li>
+                <li
+                  className={
+                    s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
+                  }
+                >
+                  Ksiązki IT
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section className={s.container__linksSection__linksWrapper}>
+            <div
+              className={s.container__linksSection__linksWrapper__sectionHeader}
+            >
+              <h3
+                className={
+                  s.container__linksSection__linksWrapper__sectionHeader__header
+                }
+              >
+                Prywatność
+              </h3>
+            </div>
+            <div
+              className={
+                s.container__linksSection__linksWrapper__sectionElementsWrapper
+              }
+            >
+              <ul
+                className={
+                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems
+                }
+              >
+                <li
+                  className={
+                    s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
+                  }
+                >
+                  Polityka Prywatności
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section className={s.container__linksSection__linksWrapper}>
+            <div
+              className={s.container__linksSection__linksWrapper__sectionHeader}
+            >
+              <h3
+                className={
+                  s.container__linksSection__linksWrapper__sectionHeader__header
+                }
+              >
+                Znajdź Mnie
+              </h3>
+            </div>
+            <div
+              className={
+                s.container__linksSection__linksWrapper__sectionElementsWrapper
+              }
+            >
+              <Image
+                className={s.container__logoSection__img}
+                src={ytIcon}
+                alt="YouTube Icon"
+                width={50}
+                height={50}
+              />
+              <Image
+                className={s.container__logoSection__img}
+                src={linkedinIcon}
+                alt="LinkedIn Icon"
+                width={50}
+                height={50}
+              />
+              <Image
+                className={s.container__logoSection__img}
+                src={githubIcon}
+                alt="GitHub icon"
+                width={47}
+                height={47}
+              />
+            </div>
+          </section>
+        </section>
       </section>
-      <section className={s.container__linksSection}>
-        <section className={s.container__linksSection__linksWrapper}>
-          <div
-            className={s.container__linksSection__linksWrapper__sectionHeader}
-          >
-            <h3
-              className={
-                s.container__linksSection__linksWrapper__sectionHeader__header
-              }
-            >
-              Linki
-            </h3>
-          </div>
-          <div
-            className={
-              s.container__linksSection__linksWrapper__sectionElementsWrapper
-            }
-          >
-            <ul
-              className={
-                s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems
-              }
-            >
-              <li
-                className={
-                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
-                }
-              >
-                Oferty Pracy
-              </li>
-              <li
-                className={
-                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
-                }
-              >
-                Kursy IT
-              </li>
-              <li
-                className={
-                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
-                }
-              >
-                Ksiązki IT
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className={s.container__linksSection__linksWrapper}>
-          <div
-            className={s.container__linksSection__linksWrapper__sectionHeader}
-          >
-            <h3
-              className={
-                s.container__linksSection__linksWrapper__sectionHeader__header
-              }
-            >
-              Prywatność
-            </h3>
-          </div>
-          <div
-            className={
-              s.container__linksSection__linksWrapper__sectionElementsWrapper
-            }
-          >
-            <ul
-              className={
-                s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems
-              }
-            >
-              <li
-                className={
-                  s.container__linksSection__linksWrapper__sectionElementsWrapper__listItems__item
-                }
-              >
-                Polityka Prywatności
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className={s.container__linksSection__linksWrapper}>
-          <div
-            className={s.container__linksSection__linksWrapper__sectionHeader}
-          >
-            <h3
-              className={
-                s.container__linksSection__linksWrapper__sectionHeader__header
-              }
-            >
-              Znajdź Mnie
-            </h3>
-          </div>
-          <div
-            className={
-              s.container__linksSection__linksWrapper__sectionElementsWrapper
-            }
-          >
-            <Image
-              className={s.container__logoSection__img}
-              src={ytIcon}
-              alt="YouTube Icon"
-              width={50}
-              height={50}
-            />
-            <Image
-              className={s.container__logoSection__img}
-              src={linkedinIcon}
-              alt="LinkedIn Icon"
-              width={50}
-              height={50}
-            />
-            <Image
-              className={s.container__logoSection__img}
-              src={githubIcon}
-              alt="GitHub icon"
-              width={50}
-              height={50}
-            />
-          </div>
-        </section>
+      <section className={s.container__copyright}>
+        <span className={s.container__copyright__content}>Copyright © 2024 Made by Oliwier Markiewicz</span>
       </section>
     </footer>
   );
