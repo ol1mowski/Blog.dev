51937b1d3fcfca3347796ca2ba7b8585724a4f81
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-05-25 11:28:56 +0200

    [HomePage] style: add new tag to update margin on Section

---
+++ app/prywatnosc/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Pages/Footer/Footer.page";
+import Header from "@/components/Pages/Header/Header.page";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  );
+}
+
+export default page;
--- components/Pages/HomePage/HomePage.page.tsx
+++ components/Pages/HomePage/HomePage.page.tsx
@@ -5,16 +5,18 @@ function HomePage() {
   return (
     <section className={s.container}>
       <Header />
-      <section className={s.container__contentSection}>
-        <span className={s.container__contentSection__category}>React</span>
-        <h1 className={s.container__contentSection__title}>
-          Jak zacząć się uczyć React w 2024 roku ?
-        </h1>
-        <p className={s.container__contentSection__description}>
-          Tutaj podzielę się z wami najlepszymi kursami i ksiązkami o tematyce
-          tworzenia stron internetowych. Koniecznie musisz je znać
-        </p>
-        <button className={s.container__contentSection__btn}>Czytaj</button>
+      <section className={s.container__content}>
+        <section className={s.container__contentSection}>
+          <span className={s.container__contentSection__category}>React</span>
+          <h1 className={s.container__contentSection__title}>
+            Jak zacząć się uczyć React w 2024 roku ?
+          </h1>
+          <p className={s.container__contentSection__description}>
+            Tutaj podzielę się z wami najlepszymi kursami i ksiązkami o tematyce
+            tworzenia stron internetowych. Koniecznie musisz je znać
+          </p>
+          <button className={s.container__contentSection__btn}>Czytaj</button>
+        </section>
       </section>
     </section>
   );
